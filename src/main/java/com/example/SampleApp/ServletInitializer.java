6fc84beada3ced6e5b1797f39cc6eb5dabb31c00
srivaniKammri <srivani.kss@gmail.com>
2021-07-12 21:47:03 +0530

    First commit

---
+++ src/main/java/com/example/SampleApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.SampleApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleAppApplication.class);
+	}
+
+}
